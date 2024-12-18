8e3447f2c70d165852e35e295bec1ebc589d6f99
Kayode <officialkayodeolowo@gmail.com>
2024-12-18 11:08:17 +0100

    updates

--- src/app/components/modals/ViewProperty.jsx
+++ src/app/components/modals/ViewProperty.jsx
@@ -31,7 +31,7 @@ const ViewProperty = ({ isOpen, onClose, id }) => {
     <>
       {isOpen && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90 overflow-y-auto scrollbar-hide">
-          <div className="bg-dark text-white p-8 rounded-md shadow-md w-[90%] max-h-[40rem] lg:w-[30rem] mt-6 scrollbar-hide overflow-y-auto">
+          <div className="bg-dark text-white p-8 rounded-md shadow-md w-[90%] max-h-[40rem] lg:w-[35rem] mt-6 scrollbar-hide overflow-y-auto">
             <button onClick={onClose} className="text-white mb-6 mr-auto text-xl">
               X
             </button>
@@ -46,8 +46,8 @@ const ViewProperty = ({ isOpen, onClose, id }) => {
                     className="w-full rounded-lg md:h-[20rem]"
                   />
                 )}
-                <h2 className="text-3xl mt-8 font-bold">{property.property_name}</h2>
-                <p>{property.address}</p>
+                <h2 className=" text-3xl mt-4 font-bold">{property.property_name}</h2>
+                <p> {`Location: ${property.address}`}</p>
                 <p>{`Price: $${property.price.toLocaleString()}`}</p>
                 <p>{`Type: ${property.property_type}`}</p>
               </div>
